417ec4732dd1f8df9ca7a1a6bcc21e44cab93945
Ford, Sam Michael <samford@sams-mbp-2.lan>
2017-10-31 17:19:04 -0400

    asdf

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from config import BaseConfig
 from flask_bcrypt import Bcrypt
 
-app = Flask(__name__, static_folder="./static/build/", template_folder="./build/")
+app = Flask(__name__, static_folder="./static/build", template_folder="./build")
 app.config.from_object(BaseConfig)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
