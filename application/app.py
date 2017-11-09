3cd388605e8303703b1f2594155193b0902e66e8
Ford, Sam Michael <samford@lawn-143-215-83-177.lawn.gatech.edu>
2017-11-09 17:16:14 -0500

    Ap stinks

--- application/app.py
+++ application/app.py
@@ -1,12 +1,12 @@
 from flask import request, render_template, jsonify, url_for, redirect, g
-# from flask.cors import CORS, cross_origin
 from .models import User
 from index import app, db
 from sqlalchemy.exc import IntegrityError
 from .utils.auth import generate_token, requires_auth, verify_token
-
-# cors = CORS(app, resources={r"/foo": {"origins": "*"}})
-# app.config['CORS_HEADERS'] = 'Content-Type'
+from sklearn.ensemble import RandomForestClassifier as rf
+import pandas as pd
+from sklearn.externals import joblib
+import json
 
 
 @app.route('/', methods=['GET'])
@@ -25,9 +25,112 @@ def get_user():
     return jsonify(result=g.current_user)
 
 @app.route("/api/tester", methods=["GET"])
-# @cross_origin(origin='*',headers=['Content-Type','Authorization'])
 def get_test():
-    return jsonify("cancer, duah")
+    response = jsonify({'death': 'cancer'})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+# inputs
+training_data = './static/public/train.csv'
+include = ['Age', 'Sex', 'Embarked', 'Survived']
+dependent_variable = include[-1]
+
+model_directory = 'model'
+model_file_name = '%s/model.pkl' % model_directory
+model_columns_file_name = '%s/model_columns.pkl' % model_directory
+
+# These will be populated at training time
+model_columns = None
+clf = None
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    print("inside predict")
+    import pandas as pd
+    from sklearn.externals import joblib
+    if clf:
+        try:
+            json_ = [
+                {'Age': 85, 'Sex': 'male', 'Embarked': 'S'},
+                {'Age': 24, 'Sex': 'female', 'Embarked': 'C'},
+                {'Age': 3, 'Sex': 'male', 'Embarked': 'C'},
+                {'Age': 21, 'Sex': 'male', 'Embarked': 'S'}
+            ]
+            # json_ = request.json
+            query = pd.get_dummies(pd.DataFrame(json_))
+
+            # https://github.com/amirziai/sklearnflask/issues/3
+            # Thanks to @lorenzori
+            query = query.reindex(columns=model_columns, fill_value=0)
+
+            prediction = list(clf.predict(query))
+            print(prediction)
+            # response = jsonify({'prediction':'predict'})
+            # response.headers.add('Access-Control-Allow-Origin', '*')
+            # return response
+            return prediction
+
+
+        except Exception:
+            print('exception')
+            # response = jsonify({'prediction': 'error'})
+            # response.headers.add('Access-Control-Allow-Origin', '*')
+            # return response
+            # return jsonify({'error': str(e), 'trace': traceback.format_exc()})
+    else:
+        print('train first')
+        # response = jsonify({'prediction': 'else'})
+        # response.headers.add('Access-Control-Allow-Origin', '*')
+        # return response
+
+@app.route('/train', methods=['GET'])
+def train():
+    # using random forest as an example
+    # can do the training separately and just update the pickles
+
+    df = pd.read_csv(training_data)
+    df_ = df[include]
+
+    categoricals = []  # going to one-hot encode categorical variables
+
+    for col, col_type in df_.dtypes.iteritems():
+        if col_type == 'O':
+            categoricals.append(col)
+        else:
+            df_[col].fillna(0, inplace=True)  # fill NA's with 0 for ints/floats, too generic
+
+    # get_dummies effectively creates one-hot encoded variables
+    df_ohe = pd.get_dummies(df_, columns=categoricals, dummy_na=True)
+
+    x = df_ohe[df_ohe.columns.difference([dependent_variable])]
+    y = df_ohe[dependent_variable]
+
+    # capture a list of columns that will be used for prediction
+    global model_columns
+    model_columns = list(x.columns)
+    # joblib.dump(model_columns, model_columns_file_name)
+
+    global clf
+    clf = rf()
+    # start = time.time()
+    clf.fit(x, y)
+    # print('Trained in %.1f seconds' % (time.time() - start))
+    # print('Model training score: %s' % clf.score(x, y))
+
+    # joblib.dump(clf, model_file_name)
+
+    print('Predict')
+    prediction = predict()
+    # convert to ints
+    d = map(lambda x: int(x), prediction)
+
+    # response = jsonify({'train':'Success'})
+    response = jsonify({'prediction': list(d)})
+    # response = jsonify(result=list(d))
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
 
 
 @app.route("/api/create_user", methods=["POST"])
--- static/src/App.js
+++ static/src/App.js
@@ -7,22 +7,52 @@ export default class App extends Component {
   constructor() {
     super()
     this.state = {
-      fakeData: null
+      death: null,
+      prediction: null
     }
   }
 
   componentDidMount() {
-    fetch("http://localhost:5000/api/tester")
+    fetch("/api/tester")
       .then(response => response.json())
       .then(data => {
         console.log(data)
         this.setState({
-          fakeData: data
+          death: data.death
         })
       })
       .catch(error => {
         console.log(error)
       })
+
+    fetch("/train")
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        if (data.prediction) {
+          console.log(data.prediction)
+          console.log("issa success - grabbing prediction")
+          this.setState({
+            prediction: data.prediction
+          })
+          // fetch("/predict")
+          //   .then(res => res.json())
+          //   .then(dat => {
+          //     console.log(dat)
+          //     this.setState({
+          //       prediction: dat.prediction
+          //     })
+          //   })
+          //   .catch(err => {
+          //     console.log(err)
+          //   })
+        }
+      })
+      .catch(error => {
+        console.log(error)
+      })
+
+    
   }
 
   render() {
@@ -30,7 +60,9 @@ export default class App extends Component {
     return (
       <div className="App">
         <h1>How ya boi die</h1>
-        <p>{this.state.fakeData ? this.state.fakeData : "loading"}</p>
+        <p>{this.state.death ? this.state.death : "loading"}</p>
+        <p>{this.state.prediction ? JSON.stringify(this.state.prediction) : "loading"}</p>
+
       </div>
     );
   }
